1aafc324070285f29427306ac62d15eefa24036f
vijay <mail.vjrao@gmail.com>
2025-04-20 11:42:10 -0400

    Updated contact email

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -32,8 +32,8 @@ export default function Home() {
                 <path d="M2.003 5.884L10 9.882l7.997-3.998A2 2 0 0016 4H4a2 2 0 00-1.997 1.884z" />
                 <path d="M18 8.118l-8 4-8-4V14a2 2 0 002 2h12a2 2 0 002-2V8.118z" />
               </svg>
-              <a href="mailto:dreamlit@icloud.com" className="hover:text-indigo-500 dark:hover:text-indigo-400">
-                dreamlit@icloud.com
+              <a href="mailto:hello@dreamlit.io" className="hover:text-indigo-500 dark:hover:text-indigo-400">
+                hello@dreamlit.io
               </a>
             </div>
           </div>
